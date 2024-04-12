665a7d4a8479eec85a7dfd0d48f7d22928bd59db
martin <martin.alvarado2002@gmail.com>
2024-04-12 22:33:58 +0200

    [26-377]: Dispatching Actions

--- src/app/store/counter.reducer.ts
+++ src/app/store/counter.reducer.ts
@@ -1,12 +1,16 @@
-import { createReducer } from '@ngrx/store';
+import { createReducer, on } from '@ngrx/store';
+import { increment } from './counter.actions';
 
 const initialState = 0;
 
-// export const counterReducer = createReducer(initialState);
+export const counterReducer = createReducer(
+  initialState,
+  on(increment, (state) => state + 1)
+);
 
 /**
  * Old original way to create a reducer.
  */
-export function counterReducer(state = initialState): number {
-  return initialState;
-}
+// export function counterReducer(state = initialState): number {
+//   return initialState;
+// }
